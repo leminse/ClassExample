cabb5c41d8abcf8f283a711f8037541c51a37729
minseo <minseo@DESKTOP-NL04HGM>
2025-06-12 14:06:26 +0900

    상속 시 생성자 예

--- Program.cs
+++ Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClassExample
 {
+    class Parent
+    {
+        public Parent()
+        {
+            Console.WriteLine("Parent()");
+        }
+
+        public Parent(int param)
+        {
+            Console.WriteLine("Parent(int param)");
+        }
+
+        public Parent(string param)
+        {
+            Console.WriteLine("Parent(string param)");
+        }
+    }
+
+    class Child : Parent
+    {
+        public Child() : base(10)
+        {
+            Console.WriteLine("Child() : base(10)");
+        }
+
+        public Child(string input) : base(input)
+        {
+            Console.WriteLine("Child(string input) : base(input)");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Child childA = new Child();
+            Child childB = new Child("string");
         }
     }
 }
